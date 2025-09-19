104cd1cb4fb249bab70fa0b3a7ddf183455a9ae9
eisakarate <87922527+eisakarate@users.noreply.github.com>
2025-09-19 06:24:14 +0000

    chore: updated subtitle

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -27,7 +27,7 @@ export function Blog() {
   return (
     <div style={{ padding: 8 }}>
       <h1>Welcome to Corgi Blog</h1>
-      <h2>By Potato Dog</h2>
+      <h2>By Potato Dog (a.k.a, pawsome corgi)</h2>
       <CreatePost />
       <br />
       <hr />
